50d07071e69fdac89afdf980a9becd009d9283e8
__coma__ <codeliner96@gmail.com>
2022-11-19 23:55:55 +0900

    Fixed bugs.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello calendars
+
+This is a simple wep application for calendar helpers test.
+
+### [Github](https://github.com/code1iners/ce1pers)
+
+### [Npm](https://www.npmjs.com/package/@ce1pers/date-helpers)
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { useEffect, useState } from "react";
-import { makeCalendar } from "@ce1pers/date-helpers";
+import { makeCalendar, getPassedTime } from "@ce1pers/date-helpers";
 import { clazz } from "@ce1pers/use-class";
 import { CalendarDateItem } from "@ce1pers/date-helpers/src/calendar-helpers/types";
+import { GetPassedTimeOutputs } from "@ce1pers/date-helpers/dist/src/shared/types";
 
 const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let intervalId: number | undefined = undefined;
 
 function App() {
   const [calendar, setCalendar] = useState<CalendarDateItem[]>([]);
   const [date, setDate] = useState<number>();
   const [month, setMonth] = useState<number>();
+  const [passed, setPassed] = useState<GetPassedTimeOutputs | null>();
+  const now = new Date();
+
+  useEffect(() => {
+    setDate(now.getDate());
+    setMonth(now.getMonth() + 1);
+
+    intervalId = window.setInterval(() => {
+      setPassed(getPassedTime(now));
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   useEffect(() => {
     const now = new Date();
@@ -22,10 +39,20 @@ function App() {
   };
 
   return (
-    <main className="h-screen flex flex-col gap-5 justify-center items-center">
-      <article className="flex items-center justify-center gap-1 tracking-wider text-lg">
-        <span>{month ?? "?"}월</span>
-        <span>{date ?? "?"}일</span>
+    <main className="h-screen flex flex-col gap-10 justify-center items-center">
+      <article className="flex flex-col items-center justify-center gap-10 tracking-wider">
+        {passed ? (
+          <div className="flex items-center gap-1 text-sm text-gray-500">
+            <span>{passed.time}</span>
+            <span>{passed.unit}</span>
+            <span>passed..</span>
+          </div>
+        ) : null}
+
+        <div className="flex items-center gap-3 text-2xl">
+          <span>{month ?? "?"} 월</span>
+          <span>{date ?? "?"} 일</span>
+        </div>
       </article>
 
       <article>
@@ -47,7 +74,7 @@ function App() {
           {calendar.map((date) => (
             <li
               className={clazz(
-                "cursor-pointer",
+                "cursor-pointer text-center",
                 date.month !== month ? "text-gray-500" : ""
               )}
               key={date.key}
